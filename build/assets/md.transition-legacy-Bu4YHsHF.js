a83f5097ed85f549fa427dd8cd31a4bc3be8b9c3
Krystell <krystellgarcia0804@gmail.com>
2024-12-06 00:47:07 -0600

    Construccion2

--- build/assets/md.transition-legacy-Bu4YHsHF.js
+++ build/assets/md.transition-legacy-Bu4YHsHF.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ujQwLT7j.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-Bcy6rDzT.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- build/assets/swipe-back-legacy-BaJGw2PG.js
+++ build/assets/swipe-back-legacy-BaJGw2PG.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-ujQwLT7j.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-Bcy6rDzT.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
